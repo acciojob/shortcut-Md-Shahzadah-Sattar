b487e2197f666a5cd2e2a3b9814fc7bbe3b8f63b
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-25 17:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+  // Check if either of the input strings is empty
+  if (str1.length === 0 || str2.length === 0) {
+    return '';
+  }
+
+  // Extract the initial letters of the strings and concatenate them
+  const initials = str1.charAt(0) + str2.charAt(0);
+
+  return initials;
 }
 
+// Examples
+console.log(shortcut("Amnesty", "International"));  // Output: 'AI'
+console.log(shortcut("Hello", "world"));             // Output: 'Hw'
+console.log(shortcut("", "International"));           // Output: ''
+console.log(shortcut("Amnesty", ""));                 // Output: ''
+
+module.exports = shortcut;
+
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
