ffdbe5d25066a4b7018dea94ee43ef41597ffb76
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-25 17:51:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-function shortcut(str1, str2) {
+function shortcut(s1, s2) {
   // Check if either of the input strings is empty
-  if (str1.length === 0 || str2.length === 0) {
+  if (s1.length === 0 || s2.length === 0) {
     return '';
   }
 
   // Extract the initial letters of the strings and concatenate them
-  const initials = str1.charAt(0) + str2.charAt(0);
+  const initials = s1.charAt(0) + s2.charAt(0);
 
   return initials;
 }
 
-// Examples
-console.log(shortcut("Amnesty", "International"));  // Output: 'AI'
-console.log(shortcut("Hello", "world"));             // Output: 'Hw'
-console.log(shortcut("", "International"));           // Output: ''
-console.log(shortcut("Amnesty", ""));                 // Output: ''
-
-module.exports = shortcut;
-
-// Do not change the code below.
+// Uncomment the code below if you want to take input from the user
 // const s1 = prompt("Enter s1:");
 // const s2 = prompt("Enter s2:");
+
+// Call the shortcut function with user input
 // alert(shortcut(s1, s2));
+
+// Alternatively, you can provide sample strings for testing
+const result = shortcut("Amnesty", "International");
+console.log(result); // This will log the result to the console
